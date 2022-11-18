edaf325569c3c00222c4f3b0e6c19e68b0a0ac2c
wangch15 <harry19910202@gmail.com>
2022-11-19 00:46:51 +0800

    電腦版完成

---
+++ js/script.js
@@ -0,0 +1,32 @@
+// ============= typing ===============//
+var typed = new Typed(".typing",{
+    strings:["","WEB DESIGN","WEB DEVELOP","GRAPHIC DESIGN","FIGURE PANIT"],
+    typeSpeed:120,
+    backSpeed:90,
+    backDelay:1500,
+    loop:true
+})
+
+// ============= dark mode ===============//
+const modeBtn = document.querySelector('.modeBtn')
+function changeMode(){
+    console.log(modeBtn);
+    document.body.classList.toggle('darkmode')
+    if(document.body.classList.contains('darkmode')){
+        modeBtn.innerHTML = 'LIGHT MODE'
+    }else{
+        modeBtn.innerHTML = 'DARK MODE'
+    }
+}
+
+// ============= swiper ===============//
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    spaceBetween: 30,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    loop:true,
+  });
